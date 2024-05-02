8ca6fca3fce3da6d8745d83ba4dd27de18d1076a
Eric <89803837+ericf1@users.noreply.github.com>
2024-05-02 14:16:05 -0500

    init
    init

---
+++ src/app/page.tsx
@@ -0,0 +1,124 @@
+"use client";
+import React from "react";
+// import { calsans } from "@/fonts/calsans";
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+import { TracingBeam } from "../app/components/tracing-beam";
+
+export default function TracingBeamDemo() {
+  return (
+    <TracingBeam className="px-6">
+      <div className="max-w-2xl mx-auto antialiased pt-4 relative">
+        {dummyContent.map((item, index) => (
+          <div key={`content-${index}`} className="mb-10">
+            <h2 className="bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4">
+              {item.badge}
+            </h2>
+
+            <p>{item.title}</p>
+
+            <div className="text-sm  prose prose-sm dark:prose-invert">
+              {item?.image && (
+                <Image
+                  src={item.image}
+                  alt="blog thumbnail"
+                  height="1000"
+                  width="1000"
+                  className="rounded-lg mb-10 object-cover"
+                />
+              )}
+              {item.description}
+            </div>
+          </div>
+        ))}
+      </div>
+    </TracingBeam>
+  );
+}
+
+const dummyContent = [
+  {
+    title: "Dec 2017: Chicago Board Options Exchange Bitcoin Futures",
+    description: (
+      <>
+        <p>
+          The Chicago Board Options Exchange (CBOE) and Chicago Mercantile
+          Exchange (CME) made a milestone contribution by establishing Bitcoin
+          futures. Futures are derivatives on securities, which in layman terms,
+          mean an asset that tracks the price of the underlying asset.
+        </p>
+        <br />
+        <p>
+          Futures are similar to betting on the future price or performance, in
+          our case, we are betting on the future price of Bitcoin. The launch of
+          futures provided a strong signal to show growing acceptance of the
+          cryptocurrency by mainstream institutions. Bitcoin was recognized as a
+          commodity similar to gold and silver.
+        </p>
+        <br />
+        <p>
+          In practical terms, Bitcoin futures provide investors with an ability
+          to hedge against market volatility and speculate on future price
+          movements without directly owning Bitcoin. This development could
+          attract more conservative investors who may have been hesitant to
+          engage with Bitcoin due to its perceived risks and lack of regulation.
+        </p>
+        <br />
+        <p>
+          The involvement of reputable exchanges like CBOE and CME lends
+          credibility to the cryptocurrency, addressing some concerns raised by
+          regulators regarding the "black market" and decentralized nature of
+          Bitcoin, which have previously hindered the approval of Bitcoin Spot
+          ETFs. The launch of Bitcoin futures represents a crucial step forward
+          in establishing Bitcoin Spot ETFs by contributing to the growing
+          acceptance and legitimacy of the cryptocurrency.
+        </p>
+      </>
+    ),
+    badge: "React",
+    image:
+      "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Jun 2018: VanEck SolidX Bitcoin Trust Rejected",
+    description: (
+      <>
+        <p>
+          Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
+          deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
+          non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
+          sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
+          velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
+          commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
+        </p>
+        <p>
+          In dolore veniam excepteur eu est et sunt velit. Ipsum sint esse
+          veniam fugiat esse qui sint ad sunt reprehenderit do qui proident
+          reprehenderit. Laborum exercitation aliqua reprehenderit ea sint
+          cillum ut mollit.
+        </p>
+      </>
+    ),
+    badge: "Changelog",
+    image:
+      "https://images.unsplash.com/photo-1519681393784-d120267933ba?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Lorem Ipsum Dolor Sit Amet",
+    description: (
+      <>
+        <p>
+          Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
+          deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
+          non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
+          sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
+          velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
+          commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
+        </p>
+      </>
+    ),
+    badge: "Launch Week",
+    image:
+      "https://images.unsplash.com/photo-1469474968028-56623f02e42e?auto=format&fit=crop&q=80&w=3506&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
